a3f080248cda376d9429f1a9fdda280cc387ddff
André Agostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2020-11-17 21:06:42 +0000

    Emplemented test exercise II

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -5,9 +5,14 @@
 
 int main()
 {
-  //init uart 
-  uart_init(UART_BASE,FREQ/BAUD);   
-  uart_printf("\n\n\nHello world!\n\n\n");
-  //char *a = malloc(10);
-  //free(a);
+  //init uart
+  uart_init(UART_BASE,FREQ/BAUD);
+  int n, x, y;
+
+  y = 0;
+  for( n = 0 ; n < 100 ; n++ ) {
+    x = rand()%4096 - 2048; // signed 12 bit random number with 0 average [-2048, 2047]
+    y = y + (x>0?x:-x); // in average y = 1024*n
+    uart_printf("n = %d |x: %d |y: %d\n", n, x, y);
+  }
 }
